590dbc82def2b4a9392354c8146fdd35cf6d77e9
calc33 <calc33@gmail.com>
2023-06-06 19:07:15 +0900

    1クラス1ファイルへ方針変更(途中)

---
+++ Db2SrcDataSet/SortOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db2Source
+{
+    public enum SortOrder
+    {
+        Asc = 0,
+        Desc = 1
+    }
+}
